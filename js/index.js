bca0596121e76c018bbd67a11904d14814da4f92
Robert Abreu <robertabreu@pursuit.org>
2019-08-05 20:21:22 -0400

    created TodoData model

--- js/index.js
+++ js/index.js
@@ -1,2 +1,34 @@
-const root = document.getElementById("root")
+// Model // state
+class TodoData {
+	constructor() {
+		this.list = JSON.parse(localStorage.getItem("todoList") || "[]");
+	}
 
+	save(value) {
+		this.list.push(value);
+		localStorage.setItem("todolist", JSON.stringify(this.list));
+	}
+
+	delete(index) {
+		this.list = this.list.slice(0, index).concat(this.list.slice(index + 1));
+		localStorage.setItem("todolist", JSON.stringify(this.list));
+	}
+}
+
+// App
+
+class App {
+    constructor(){
+        // Initialize Model
+        this.todos = new TodoData();
+    }
+}
+
+const data = new TodoData();
+data.save("Hi");
+data.save("lol");
+console.log(data.list);
+
+data.delete(1);
+console.log(data.list);
+const root = document.getElementById("root");
