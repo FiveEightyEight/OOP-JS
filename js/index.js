b114fbdf8398334f61fa02f367901f61e7dfc3aa
Robert Abreu <robertabreu@pursuit.org>
2019-08-05 20:33:01 -0400

    added views to app class

--- js/index.js
+++ js/index.js
@@ -17,31 +17,65 @@ class TodoData {
 
 // View
 class View {
-    constructor(name, render) {
-        this.name = name;
-        this.render = render;
-    }
+	constructor(name, render) {
+		this.name = name;
+		this.render = render;
+	}
 }
 
 // App
 class App {
-    constructor(){
-        // Initialize Model
-        this.todos = new TodoData();
-        // My list of view
-        this.views = [];
-    }
+	constructor() {
+		// Initialize Model
+		this.todos = new TodoData();
+		// My list of view
+		this.views = [];
+	}
 
-    intializeViews() {
-
-    }
+	intializeViews() {}
 }
 
-const data = new TodoData();
-data.save("Hi");
-data.save("lol");
-console.log(data.list);
+const app = App();
 
-data.delete(1);
-console.log(data.list);
+app.addView(
+	"header",
+	`
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Todo List App</h1>
+    </div>
+</div>
+`,
+);
+
+app.addView(
+	"input",
+	`
+<div class="input-group mb-3">
+    <input
+    type="text"
+    class="js-text-input form-control"
+    placeholder="Add..."
+    />
+    <div class="input-group-append">
+        <button
+        class="js-add btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+        >
+        Add
+        </button>
+    </div>
+</div>
+`,
+);
+
+app.addView(
+	"list",
+	`
+<ul class="list-group mx-3">
+	<li class="list-group-item" data-i="0">Chicken</li>
+</ul>
+`,
+);
 const root = document.getElementById("root");
